23676d768d13dedef37faecb2910d4e2c7797f77
Vlad Bakin <v.bakin@semrush.com>
2015-04-06 17:23:03 +0300

    Increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='falcon-wing',
-    version='0.1.4',
+    version='0.1.5',
     description='Falcon extension for building nice looking API',
 
     author='Vlad Bakin',
